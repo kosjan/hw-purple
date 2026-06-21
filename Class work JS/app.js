909a02049070f111e3f15298736efede9aa1b516
AkishevKA <akishevka@mosreg.ru>
2026-06-21 22:09:01 +0300

    add class work for if else

--- Class work JS/app.js
+++ Class work JS/app.js
@@ -1,6 +1,6 @@
 
 // Data
-const paymentHour = 80;
+/*const paymentHour = 80;
 const workProj = 40;
 
 // Решение
@@ -15,4 +15,34 @@ else {
     console.log(true);
     console.log(workProj * paymentHour);
 
-}
+}*/
+
+// Сложный процент
+
+// let balance = 12000;
+// let procent = 0.07;
+
+// let sum = balance * (1 + procent / 12) ** 24;
+
+// if (sum > 13500) {
+//     let template = `Дом можно купить, остаток после покупки ${parseInt(sum - 13500)} USD`;
+//     console.log(template);
+// }
+// else {
+//     console.log('Дом купить нельзя, сумма вклада - ' + sum + ' USD');
+// }
+
+// Robot check
+
+// const answer = prompt('Сколько будет 7 + или - 15?')
+
+// let message;
+
+// if (Number(answer) === 22 || Number(answer) === -8) {
+//     console.log('Успех')
+// } else if (answer === 'Я не робот') {
+//     console.log('Успех')
+// }
+// else {
+//     console.log('Вы робот!')
+// }
